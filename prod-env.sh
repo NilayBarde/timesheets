8d50593018630c0e31d3f59897bd310a9601d66d
Timesheets <timesheets@guest.guest>
2019-10-29 21:42:03 +0000

    Gitignore Temp

---
+++ prod-env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export MIX_ENV=prod
+export PORT=5001
+export DATABASE_URL="ecto://timesheets:TkzW6zZRSeqZk86r@localhost/timesheets_prod"
+export SECRET_KEY_BASE="HaXzM3V4H/Syt0iYufFj1M3v4ysxzkUONjAPrDzA7NJxKUracWBLSJc7chFdUk6"
+
+
