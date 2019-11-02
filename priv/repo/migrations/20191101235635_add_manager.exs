0fbfa4b0dcf65700ddef289a4e44382fe036aab5
Timesheets <timesheets@guest.guest>
2019-11-02 01:14:01 +0000

    Bug Fixes

--- priv/repo/migrations/20191101235635_add_manager.exs
+++
@@ -1,9 +0,0 @@
-defmodule Timesheets.Repo.Migrations.AddManager do
-  use Ecto.Migration
-
-  def change do
-    alter table("users") do
-      add :manager_id, references(:managers)
-    end
-  end
-end
